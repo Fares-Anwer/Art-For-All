84e7d1cea30fca345abf1c12f29abcbe1a302330
Fares-Anwer <143360028+Fares-Anwer@users.noreply.github.com>
2024-11-15 04:48:08 +0300

    fix some style

--- customer/my_account.php
+++ customer/my_account.php
@@ -1,5 +1,6 @@
 <?php require_once 'includes/header.php'; ?>
 
+<?php require_once '../includes/sidebar_for_all.php'; ?>
 <?php
 
 
@@ -13,43 +14,8 @@ if (!isset($_SESSION['customer_email'])) {
 	header('Location: ../checkout.php');
 	exit();
 }
+
 ?>
-
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top" id="navbar">
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-		<ul class="navbar-nav mr-auto text-uppercase">
-			<li>
-				<a href="../index.php">Home</a>
-			</li>
-			<li>
-				<a href="../shop.php">Marketplace</a>
-			</li>
-			<li>
-				<a class="active" href="my_account.php">My Account</a>
-			</li>
-			<li>
-				<a href="../cart.php">Shopping Cart</a>
-			</li>
-			<li>
-				<a href="../contact.php">Contact Us</a>
-			</li>
-		</ul>
-		<?php if (isset($_SESSION['customer_email'])): ?>
-			<a href="../cart.php" class="btn btn-success mr-2"><i class="fas fa-shopping-cart"></i><span> <?php echo $getFromU->count_product_by_ip($ip_add); ?> items in Cart</span></a>
-		<?php else: ?>
-			<a href="../cart.php" class="btn btn-success mr-2"><i class="fas fa-shopping-cart"></i><span> 0 items in Cart</span></a>
-		<?php endif ?>
-
-		<form class="form-inline my-2 my-lg-0">
-			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="user_query" required="1">
-			<button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="search">Search</button>
-		</form>
-	</div>
-</nav>
 
 
 <div id="content">
@@ -200,4 +166,4 @@ if (!isset($_SESSION['customer_email'])) {
 	</div>
 </div>
 
-<?php require_once 'includes/footer.php'; ?>
+<?php require_once '../includes/footer.php'; ?>
